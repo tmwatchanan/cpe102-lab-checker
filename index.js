11e509f60deeb952d2bafe9322d28d05b8eb0845
tmwatchanan <tmwatchanan@gmail.com>
2018-01-14 01:00:41 +0700

    Add getting some part of content from README.md file

--- index.js
+++ index.js
@@ -25,9 +25,12 @@ app.listen(app.get('port'), () => {
 
 var GitHubApi = require('github');
 var request = require('request');
+var rp = require('request-promise');
 var markdown = require('markdown').markdown;
 var path = require('path');
 var marked = require('marked');
+var Promise = require("bluebird");
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -52,54 +55,49 @@ app.get('/CPE-CMU-26/pulls', function (req, res) {
     }).then(result => {
         const pullRequests = result.data;
         var pullRequestList = [];
+        var ps = [];
+        var readmeMdList = [];
         pullRequests.forEach(pullRequest => {
             let objPullRequest = {
                 number: pullRequest.number,
                 id: pullRequest.id,
                 title: pullRequest.title,
                 user: pullRequest.user.login,
-                created_at: pullRequest.created_at
+                created_at: pullRequest.created_at,
+                readme_link: req.get('host') + '/CPE-CMU-26/readme/' + pullRequest.user.login
             };
-            request('https://raw.githubusercontent.com/' + objPullRequest.user  + '/CPE-CMU-26/master/README.md', function (error, response, body) {
-                let answerMd = body.substring(body.lastIndexOf(headMd) + headMd.length, body.indexOf("---\n"));
-                objPullRequest.readme_md = marked(answerMd);
-            });
+            // ps.push(rp('https://raw.githubusercontent.com/' + objPullRequest.user + '/CPE-CMU-26/master/README.md'));
+            // rp('https://raw.githubusercontent.com/' + objPullRequest.user + '/CPE-CMU-26/master/README.md')
+            //     .then(function (htmlString) {
+            //         let answerMd = htmlString.substring(htmlString.lastIndexOf(headMd) + headMd.length, htmlString.indexOf("---\n"));
+            //         objPullRequest.readme_md = marked(answerMd);
+            //     });
             pullRequestList.push(objPullRequest);
+            // console.log(objPullRequest);
         });
+        // After loop
+        // ps = ps.map((promise) => promise.reflect());
+
+        // Promise.all(ps)
+        //     .each(pInspection => {
+        //         if (pInspection.isFulfilled()) {
+        //             readmeMdList.push(pInspection.value())
+        //         } else {
+        //             console.log(pInspection.reason());
+        //         }
+        //     })
+        //     .then(() => callback(readmeMdList)); // Or however you want to proceed
+
         // return res.send(pullRequestList);
         res.render('cpe-cmu-26-pulls', {
             pullRequestList: pullRequestList
         });
     });
-
-    // github.repos.getContent({
-    //     owner: 'tmwatchanan',
-    //     repo: 'CPE-CMU-26',
-    //     path: 'README.md'
-    //     // per_page: 100,
-    //     // page: 1
-    // }).then(result => {
-    //     return res.send(result);
-    // })
-    // }).then(result => {
-    //     const pullRequests = result.data;
-    //     var pullRequestList = [];
-    //     pullRequests.forEach(pullRequest => {
-    //         // let objPullRequest = {
-    //         //     number: pullRequest.number,
-    //         //     title: pullRequest.title,
-    //         //     user: pullRequests.user.login,
-    //         //     created_at: pullRequests.created_at,
-
-    //         // };
-    //     });
-    //     return res.send(pullRequests);
-    // });
 });
 
-app.get('/CPE-CMU-26/files', function (req, res) {
+app.get('/CPE-CMU-26/readme/:user', function (req, res) {
 
-    request('https://raw.githubusercontent.com/peerapas/CPE-CMU-26/master/README.md', function (error, response, body) {
+    request('https://raw.githubusercontent.com/' + req.params.user  + '/CPE-CMU-26/master/README.md', function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         // console.log('body:', body); // Print the HTML for the Google homepage.

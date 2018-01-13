b68b9e5a56930a87c46ffa0f439fc655af91abd4
tmwatchanan <tmwatchanan@gmail.com>
2018-01-13 17:57:04 +0700

    Add an endpoint that get all pull requests from specific repo and user

--- index.js
+++ index.js
@@ -21,6 +21,79 @@ app.listen(app.get('port'), () => {
     console.log('Listening requests on port ' + port);
 });
 
-app.get('/github', function (req, res) {
-    res.send('You are at /github endpoint.');
+// ---------------------------------------------------------------------------------------------
+
+var GitHubApi = require('github');
+var request = require('request');
+
+const github = new GitHubApi({
+    debug: true
+});
+
+app.get('/CPE-CMU-26/pulls', function (req, res) {
+    // res.send('You are at /github endpoint.');
+    github.pullRequests.getAll({
+        owner: 'tmwatchanan',
+        repo: 'CPE-CMU-26',
+        per_page: 100,
+        page: 1,
+        direction: 'asc'
+    }).then(result => {
+        const pullRequests = result.data;
+        return res.send(pullRequests);
+        var pullRequestList = [];
+        pullRequests.forEach(pullRequest => {
+            let objPullRequest = {
+                number: pullRequest.number,
+                id: pullRequest.id,
+                title: pullRequest.title,
+                user: pullRequest.user.login,
+                created_at: pullRequest.created_at
+            };
+            pullRequestList.push(objPullRequest);
+        });
+        return res.send(pullRequestList);
+    });
+
+    // github.repos.getContent({
+    //     owner: 'tmwatchanan',
+    //     repo: 'CPE-CMU-26',
+    //     path: 'README.md'
+    //     // per_page: 100,
+    //     // page: 1
+    // }).then(result => {
+    //     return res.send(result);
+    // })
+    // }).then(result => {
+    //     const pullRequests = result.data;
+    //     var pullRequestList = [];
+    //     pullRequests.forEach(pullRequest => {
+    //         // let objPullRequest = {
+    //         //     number: pullRequest.number,
+    //         //     title: pullRequest.title,
+    //         //     user: pullRequests.user.login,
+    //         //     created_at: pullRequests.created_at,
+
+    //         // };
+    //     });
+    //     return res.send(pullRequests);
+    // });
+});
+
+app.get('/CPE-CMU-26/files', function (req, res) {
+
+    request('https://raw.githubusercontent.com/peerapas/CPE-CMU-26/master/README.md', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+        return res.send(body);
+    });
+
+    // github.repos.get({
+    //     owner: 'peerapas',
+    //     repo: 'CPE-CMU-26'
+    // })
+    // .then(result => {
+    //     return res.send(result);
+    // });
 });

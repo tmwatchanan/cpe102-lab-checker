5a8156ec2c877c1b961b23f77f7f3a1620bc9a1d
tmwatchanan <tmwatchanan@gmail.com>
2018-01-25 21:17:56 +0700

    Change port number

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser'); // handling HTML body
 var morgan = require('morgan'); // logging
 
-var port = process.env.PORT || 8080; // process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 5050; // process.env.PORT lets the port be set by Heroku
 app.set('port', port);
 
 // Allow CORS
@@ -16,10 +17,57 @@ app.use(function (req, res, next) {
     next();
 });
 
+// use body parser so we can get info from POST and/or URL params
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
 app.use(morgan('dev'));
 app.listen(app.get('port'), () => {
     console.log('Listening requests on port ' + port);
 });
+
+// ---------------------------------------------------------------------------------------------
+
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert');
+ 
+// var cloud = true;
+ 
+// var mongodbHost = '127.0.0.1';
+// var mongodbPort = '27017';
+ 
+// var authenticate ='';
+// //cloud
+// if (cloud) {
+//  mongodbHost = 'ds113648.mlab.com';
+//  mongodbPort = '13648';
+//  authenticate = process.env.mLabCpe102User + ':' + process.env.mLabCpe102User
+// }
+ 
+// var mongodbDatabase = 'cpe102-2560-2';
+ 
+// // connect string for mongodb server running locally, connecting to a database called test
+// var url = 'mongodb://'+authenticate+'@'+mongodbHost+':'+mongodbPort + '/' + mongodbDatabase;
+ 
+// // find and CRUD: http://mongodb.github.io/node-mongodb-native/2.0/tutorials/crud_operations/
+// // aggregation: http://mongodb.github.io/node-mongodb-native/2.0/tutorials/aggregation/
+ 
+// MongoClient.connect(url, function(err, db) {
+//    assert.equal(null, err);
+//    console.log("Connected correctly to server.");
+// //var cursor = collection.find({});
+//     // find top 20 countries by  size
+//     db.collection('countries').find({},{"sort": [["area",-1]]}).limit(20).toArray(function(err, results){
+//     console.log("Country One " +JSON.stringify(results[0])); 
+//     console.log("Name of Country Four " +results[3].name+ " and size: " +results[3].area);
+ 
+//       db.close();
+//       console.log("Connection to database is closed.");
+//     });
+ 
+// }) //connect()
 
 // ---------------------------------------------------------------------------------------------
 
@@ -128,4 +176,41 @@ app.get('/cpe102-2560-2/mu', function (req, res) {
     // }).then(result => {
     //     res.send(result);
     // });
+});
+
+app.get('/cpe102-2560-2/add-students', function (req, res) {
+    const studentUsernameList = ["chawitW", "guylaxy31", "p39149845", "jackthep", "spc009", "magnet327", "TouchySarun", "natchap0n", "macropusgiganteus", "greede14", "petpusin", "armarnan12@hotmail.com", "armarnan12", "Mindji", "mickche1996", "admirerate", "job25721", "Elevator", "Annopktz", "Chayootpong", "Jinojanashi", "ghinida", "ananaum", "peerapas", "KanapongP", "poom12345p", "tomta555", "tungkraiwit", "Pleumrunner", "forceframe", "Dream2382", "600610721", "wavewcy", "ekawitjaidee", "AphisitKh", "pchunya", "darapassara", "Bright2542za", "natchapon12", "Lydiawm", "m4nucho", "Ganicknan", "vachi772", "Lydiawimoo", "thanaphon0737", "phumthai", "sasirat", "apisaraT", "praewpsl", "hutchiw", "HeyzzWatcharin", "knightg1000", "attakorn0798", "songcramlok", "jnpat", "arpologize", "natapong87", "mangocpe1196", "pattadonnutes", "noratap09", "khaohoms97", "phanthakan0759", "Gamecup", "supakonzz", "Thipaporn", "Tiewly", "Acexakino", "Krisanawan19", "Nutchapun", "Rattatammanoon", "Sirin0786", "Boomlnwza0799", "st203g1", "teerapatpor", "thutgtz", "AoFNontawat", "notkakmak", "TimingKidz", "Faikavi", "Flukkyeieizaa", "zenoice", "Nuttakun", "BestPucharapol", "kawinupra", "ravichahashi", "foxxeses23", "midoriz", "kiujkio", "bombbae1234", "600610794", "PondPang", "Whaleb", "mrkwskiti", "Tontae", "PacharaBu", "terkzayo", "KotoriSN", "Pattadon ", "teamhd35975", "pakhatai"];
+
+    github.authenticate({
+        type: 'token',
+        token: process.env.githubToken
+    });
+
+    var resultList = [];
+    studentUsernameList.forEach(studentUsername => {
+        github.orgs.addOrgMembership({
+            org: 'cpe102-2560-2',
+            username: studentUsername,
+            role: 'member'
+        }).then(result => {
+            // console.log(result);
+            const resultObject = {
+                username: result.data.user.login,
+                state: result.data.state,
+                org: result.data.organization.login
+            }
+            const resultString = resultObject.username + " has been invited to " + resultObject.org + ". [" + resultObject.state + "]";
+            console.log(resultString);
+            resultList.push(resultString);
+        });
+    });
+    return res.send({ resultList });
+});
+
+app.post('/cpe102-2560-2/exam-i', function (req, res) {
+    console.log(req.body);
+    console.log(req.body.results);
+    console.log(req.body.id);
+    console.log(req.body.token);
+    return res.send(req.body);
 });
